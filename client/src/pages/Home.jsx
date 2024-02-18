e2284990397ef8643f137a08cd8d594cbd854ef9
Mayank2112 <30808mayank21@gmail.com>
2024-02-18 12:03:24 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import PostCard from '../components/PostCard';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 text-4xl lg:text-6xl font-bold dark:text-slate-200'>Welcome to my Blog</h1>
+        <div className='text-gray-400 text-xl sm:text-sm'>
+          Here you'll find a variety of articles and tutorials on topic such as web development, software engineering, and programming languages.  
+        </div>
+        <Link to={'/search'} className='text-xs sm:text-sm text-teal-700 font-bold hover:underline'>
+          View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((p) => 
+                <PostCard key={p._id} p={p} />
+              )}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
